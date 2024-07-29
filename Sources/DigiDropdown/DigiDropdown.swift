dd3fcea75b337b8ba8734d11f235d6d2fc30455c
Ram <rnrrajput84@gmail.com>
2024-07-29 09:03:25 +0530

    add dropdown

--- Sources/DigiDropdown/DigiDropdown.swift
+++ Sources/DigiDropdown/DigiDropdown.swift
@@ -2,78 +2,95 @@
 // https://docs.swift.org/swift-book
 
 import SwiftUI
+
 public struct  DigiDropdown: View {
-    
     let options: [String]
     var menuWdith: CGFloat  =  150
     var buttonHeight: CGFloat  =  50
     var maxItemDisplayed: Int  =  3
-    
     @Binding  var selectedOptionIndex: Int
     @Binding  var showDropdown: Bool
     @State  private  var scrollPosition: Int?
+    public init(options: [String], menuWdith: CGFloat, buttonHeight: CGFloat, maxItemDisplayed: Int, selectedOptionIndex: Binding<Int>, showDropdown: Binding<Bool>, scrollPosition: Int? = nil) {
+        self.options = options
+        self.menuWdith = menuWdith
+        self.buttonHeight = buttonHeight
+        self.maxItemDisplayed = maxItemDisplayed
+        _selectedOptionIndex = selectedOptionIndex
+        _showDropdown = showDropdown
+        self.scrollPosition = scrollPosition
+    }
     
     public var body: some  View {
         
         VStack {
             
-            VStack(spacing: 0) {
-                if (showDropdown) {
-                    let scrollViewHeight: CGFloat  = options.count > maxItemDisplayed ? (buttonHeight*CGFloat(maxItemDisplayed)) : (buttonHeight*CGFloat(options.count))
-                    ScrollView {
-                        LazyVStack(spacing: 0) {
-                            ForEach(0..<options.count, id: \.self) { index in
-                                Button(action: {
-                                    withAnimation {
-                                        selectedOptionIndex = index
-                                        showDropdown.toggle()
-                                    }
-                                    
-                                }, label: {
-                                    HStack {
-                                        Text(options[index])
-                                        Spacer()
-                                        if (index == selectedOptionIndex) {
-                                            Image(systemName: "checkmark.circle.fill")
+            if #available(macOS 12.0, *) {
+                VStack(spacing: 0) {
+                    if (showDropdown) {
+                        let scrollViewHeight: CGFloat  = options.count > maxItemDisplayed ? (buttonHeight*CGFloat(maxItemDisplayed)) : (buttonHeight*CGFloat(options.count))
+                        ScrollView {
+                            if #available(macOS 11.0, *) {
+                                LazyVStack(spacing: 0) {
+                                    ForEach(0..<options.count, id: \.self) { index in
+                                        Button(action: {
+                                            withAnimation {
+                                                selectedOptionIndex = index
+                                                showDropdown.toggle()
+                                            }
                                             
-                                        }
+                                        }, label: {
+                                            HStack {
+                                                Text(options[index])
+                                                Spacer()
+                                                if (index == selectedOptionIndex) {
+                                                    Image(systemName: "checkmark.circle.fill")
+                                                    
+                                                }
+                                            }
+                                            
+                                        })
+                                        .padding(.horizontal, 20)
+                                        .frame(width: menuWdith, height: buttonHeight, alignment: .leading)
+                                        
                                     }
-                                    
-                                })
-                                .padding(.horizontal, 20)
-                                .frame(width: menuWdith, height: buttonHeight, alignment: .leading)
-                                
+                                }
+                            } else {
+                                // Fallback on earlier versions
                             }
                         }
-                        .scrollTargetLayout()
+                        .frame(height: scrollViewHeight)
+                        .onAppear {
+                            scrollPosition = selectedOptionIndex
+                        }
+                        
                     }
-                    .scrollPosition(id: $scrollPosition)
-                    .scrollDisabled(options.count <=  3)
-                    .frame(height: scrollViewHeight)
-                    .onAppear {
-                        scrollPosition = selectedOptionIndex
-                    }
+                    // selected item
+                    Button(action: {
+                        withAnimation {
+                            showDropdown.toggle()
+                        }
+                    }, label: {
+                        HStack(spacing: nil) {
+                            Text(options[selectedOptionIndex])
+                            Spacer()
+                            if #available(macOS 11.0, *) {
+                                Image(systemName: "chevron.up")
+                                    .rotationEffect(.degrees((showDropdown ?  -180 : 0)))
+                            } else {
+                                // Fallback on earlier versions
+                            }
+                        }
+                    })
+                    .padding(.horizontal, 20)
+                    .frame(width: menuWdith, height: buttonHeight, alignment: .leading)
                     
                 }
-                // selected item
-                Button(action: {
-                    withAnimation {
-                        showDropdown.toggle()
-                    }
-                }, label: {
-                    HStack(spacing: nil) {
-                        Text(options[selectedOptionIndex])
-                        Spacer()
-                        Image(systemName: "chevron.up")
-                            .rotationEffect(.degrees((showDropdown ?  -180 : 0)))
-                    }
-                })
-                .padding(.horizontal, 20)
-                .frame(width: menuWdith, height: buttonHeight, alignment: .leading)
-                
+                .foregroundStyle(Color.white)
+                .background(RoundedRectangle(cornerRadius: 16).fill(Color.black))
+            } else {
+                // Fallback on earlier versions
             }
-            .foregroundStyle(Color.white)
-            .background(RoundedRectangle(cornerRadius: 16).fill(Color.black))
             
         }
         .frame(width: menuWdith, height: buttonHeight, alignment: .bottom)
